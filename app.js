48fec5319bc07becf5d98a7e28bc7a7008ce0c52
Mourya20 <mouryavignesh20@gmail.com>
2026-04-04 05:13:32 +0530

    Updated app.js to render the apis efficiently

--- app.js
+++ app.js
@@ -2,33 +2,63 @@ require('dotenv').config();
 const express = require('express');
 const rateLimit = require('./src/middleware/rateLimit');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 const app = express();
 
 app.use(express.json());
 app.use(rateLimit);
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./config/swagger'); //swagger file
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// routes
-app.use('/auth', require('./src/routes/auth.routes'));
-app.use('/records', require('./src/routes/record.routes'));
-app.use('/dashboard', require('./src/routes/dashboard.routes'));
-app.use('/budget', require('./src/routes/budget.routes'));
 
-// logger
+//LOGGER (put BEFORE routes)
+
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
 
-// error handler
+//SWAGGER 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Finance Backend API",
+      version: "1.0.0",
+      description: "Finance Management System with RBAC"
+    },
+    servers: [
+      {
+        url: "https://finance-data-processing-access-control-lxp4.onrender.com"
+      }
+    ]
+  },
+  apis: [] // optional
+};
+
+const swaggerSpec = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// ROUTES
+app.use('/auth', require('./src/routes/auth.routes'));
+app.use('/records', require('./src/routes/record.routes'));
+app.use('/dashboard', require('./src/routes/dashboard.routes'));
+app.use('/budget', require('./src/routes/budget.routes'));
+
+//ROOT 
+app.get('/', (req, res) => {
+  res.send("API Running");
+});
+
+// 404 HANDLER
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// ERROR HANDLER 
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: "Something went wrong" });
 });
 
-// health route
-app.get('/', (req, res) => {
-  res.send("API Running");
-});
 module.exports = app;
