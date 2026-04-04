b0bcd79e44e065c9cfac99d8830d5ddf299abe7f
Mourya20 <mouryavignesh20@gmail.com>
2026-04-04 10:04:11 +0530

    Rectified error in record.service

--- src/services/record.service.js
+++ src/services/record.service.js
@@ -2,7 +2,13 @@ const prisma = require('../utils/prisma');
 
 exports.createRecord = async (data) => {
   return prisma.record.create({
-    data
+    data: {
+      ...data,
+      date: data.date ? new Date(data.date) : new Date()
+    }
   });
 };
-exports.getRecords = (query) => prisma2.record.findMany(query);
+
+exports.getRecords = (query) => {
+  return prisma.record.findMany(query);
+};
